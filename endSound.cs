da694665c9420c7fdcc18571a81eee158f8d9b9d
devjisong <92451281+devjisong@users.noreply.github.com>
2022-01-29 23:00:18 +0900

    endSound

---
+++ endSound.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class endSound: MonoBehaviour
+{
+    public AudioClip soundEnd;
+    AudioSource myAudio;
+    public static endSound instance;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (endSound.instance == null)
+        {
+            endSound.instance = this;
+        }
+    }
+    void Start()
+    {
+        myAudio = this.gameObject.GetComponent<AudioSource>();
+    }
+    public void PlaySound()
+    {
+        myAudio.PlayOneShot(soundEnd);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
