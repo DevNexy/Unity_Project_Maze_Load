2af7d576961276dd9278fe122907998158f59c04
devjisong <92451281+devjisong@users.noreply.github.com>
2022-01-29 23:18:46 +0900

    MenuCtrl
    Code for controlling the main menu.

---
+++ MenuCtrl.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MenuCtrl : MonoBehaviour
+{
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void StartGame()
+    {
+        SceneManager.LoadScene("GameScene");
+        AudioSource AS = GetComponent<AudioSource>();
+        AS.Play();
+    }
+    public void InfoView()
+    {
+        SceneManager.LoadScene("InfoScene");
+        AudioSource AS = GetComponent<AudioSource>();
+        AS.Play();
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+    public void MenuView()
+    {
+        SceneManager.LoadScene("MenuScene");
+        AudioSource AS = GetComponent<AudioSource>();
+        AS.Play();
+    }
+}
