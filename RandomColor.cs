3004acc2e2094d35bbf60909e2f7881284815907
devjisong <92451281+devjisong@users.noreply.github.com>
2022-01-29 23:20:33 +0900

    RandomColor

---
+++ RandomColor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomColor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<Renderer>().material.color = new Color(Random.Range(0, 255) / 255f, Random.Range(0, 255) / 255f, Random.Range(0, 255) / 255f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+
+    }
+}
