612e8e8986cbab2e6cf66376dca18793874c23d5
devjisong <92451281+devjisong@users.noreply.github.com>
2022-01-29 22:59:59 +0900

    EndPointCtrl
    Code for EndPoint control.

---
+++ EndPointCtrl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndPointCtrl : MonoBehaviour
+{
+    public Collider endPointCol;
+    // Start is called before the first frame update
+    void Start()
+    {
+        endPointCol.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(null == GameObject.FindWithTag("Coin"))
+        {
+            endPointCol.enabled = true;
+        }
+        
+    }
+}
