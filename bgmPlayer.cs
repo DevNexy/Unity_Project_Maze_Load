664f74d688909b83a008f4ad1ec175136c9fc633
devjisong <92451281+devjisong@users.noreply.github.com>
2022-01-29 22:55:02 +0900

    bgmPlayer
    Script for random background music play.

---
+++ bgmPlayer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bgmPlayer : MonoBehaviour
+{
+    public AudioClip[] Music = new AudioClip[5];
+    AudioSource AS;
+
+    void Awake()
+    {
+        AS = this.GetComponent<AudioSource>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!AS.isPlaying)
+        {
+            RandomPlay();
+        }
+    }
+    void RandomPlay()
+    {
+        AS.clip = Music[Random.Range(0, Music.Length)];
+        AS.Play();
+    }
+}
