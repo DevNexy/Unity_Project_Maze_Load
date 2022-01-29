03692bb61018ff08a989110ee14b25e832ec0470
devjisong <92451281+devjisong@users.noreply.github.com>
2022-01-29 23:20:50 +0900

    trapSound

---
+++ trapSound.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trapSound : MonoBehaviour
+{
+    public AudioClip soundTrap;
+    AudioSource myAudio;
+    public static trapSound instance;
+    void Awake()
+    {
+        if (trapSound.instance == null)
+        {
+            trapSound.instance = this;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAudio = this.gameObject.GetComponent<AudioSource>();
+    }
+    public void PlaySound()
+    {
+        myAudio.PlayOneShot(soundTrap);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
