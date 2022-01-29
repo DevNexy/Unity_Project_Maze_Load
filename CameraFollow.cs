f1a90778df52577bed4f8873c851d46b73636831
devjisong <92451281+devjisong@users.noreply.github.com>
2022-01-29 22:55:39 +0900

    CameraFollow
    Look at the camera.

---
+++ CameraFollow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+
+    private Transform camTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        camTransform = GetComponent<Transform>();
+    }
+    void LateUpdate()
+    {
+        camTransform.position = new Vector3(target.position.x, target.position.y, target.position.z);
+
+        camTransform.LookAt(target);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
