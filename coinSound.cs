aea49288f9b0ddaa7cdc25c100895f894c8bdd77
devjisong <92451281+devjisong@users.noreply.github.com>
2022-01-29 22:59:17 +0900

    coinSound

---
+++ coinSound.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coinSound : MonoBehaviour
+{
+    public AudioClip soundCoin;
+    AudioSource myAudio;
+    public static coinSound instance;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if(coinSound.instance == null)
+        {
+            coinSound.instance = this;
+        }    
+    }
+    void Start()
+    {
+        myAudio = this.gameObject.GetComponent<AudioSource>();
+    }
+    public void PlaySound()
+    {
+        myAudio.PlayOneShot(soundCoin);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
